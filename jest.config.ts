e3afec0a052c15ec30f3ad97c4bc3a876b85c59d
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-01-31 16:30:34 -0300

    fix: update jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,10 @@
 
-export default {
-  roots:['<rootDir>/src'],
+module.exports = {
+  roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'], 
   coverageDirectory: "coverage",
   testEnvironment: "node",
   transform: {
-    '.+\\.ts$': 'ts-jest',
-  },  
+    '.+\\.ts$': 'ts-jest'
+  }  
 };
