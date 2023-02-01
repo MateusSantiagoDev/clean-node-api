e07c0e04f0e780acddc1e7708835105dd32cc099
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-01 14:00:44 -0300

    feat: ensure SignUpController calls addAcount with xorrect values

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,12 +1,18 @@
-import { HttpResponse, HttpRequest, Controller, EmailValidator } from "../protocols";
+import {
+  HttpResponse,
+  HttpRequest,
+  Controller,
+  EmailValidator,
+} from "../protocols";
 import { MissingParamError, InvalidParamError } from "../errors";
 import { badRequest, serverError } from "../helpers/http-helper";
-;
-
+import { AddAccount } from "../../domain/usecase/add-account";
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator;
-  constructor(emailValidator: EmailValidator) {
+  private readonly addAccount: AddAccount;
+  constructor(emailValidator: EmailValidator, addAccount: AddAccount) {
     this.emailValidator = emailValidator;
+    this.addAccount = addAccount;
   }
   handle(httpRequest: HttpRequest): HttpResponse {
     try {
@@ -14,7 +20,7 @@ export class SignUpController implements Controller {
         "name",
         "email",
         "password",
-        "passwordConfirmation"
+        "passwordConfirmation",
       ];
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
@@ -22,19 +28,23 @@ export class SignUpController implements Controller {
         }
       }
 
-      const { email, password, passwordConfirmation } = httpRequest.body
+      const { name, email, password, passwordConfirmation } = httpRequest.body;
+
+      this.addAccount.add({
+        name,
+        email,
+        password,
+      });
 
       const isValid = this.emailValidator.isValid(email);
       if (!isValid) {
         return badRequest(new InvalidParamError("email"));
       }
-
-      if(password !== passwordConfirmation) {
-          return badRequest(new InvalidParamError("passwordConfirmation"));
+      if (password !== passwordConfirmation) {
+        return badRequest(new InvalidParamError("passwordConfirmation"));
       }
-
     } catch (error) {
-      return serverError()
+      return serverError();
     }
   }
 }
